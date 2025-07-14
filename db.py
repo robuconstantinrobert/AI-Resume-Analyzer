11b799b4f6e1baa12fc92f55098f06b25dd0fe56
Robu Constantin Robert <robuconstantin20@stud.ase.ro>
2025-07-14 17:43:08 +0300

    Added support for render deployment

--- db.py
+++ db.py
@@ -1,12 +1,15 @@
 import psycopg2
 from pgvector.psycopg2 import register_vector
 import os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=".env")
 
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
+DB_SSLMODE= os.getenv("DB_SSLMODE", "require")
 
 def get_conn():
     conn = psycopg2.connect(
@@ -14,7 +17,8 @@ def get_conn():
         port=DB_PORT,
         dbname=DB_NAME,
         user=DB_USER,
-        password=DB_PASS
+        password=DB_PASS,
+        sslmode=DB_SSLMODE
     )
     register_vector(conn)
     return conn
