eba2c5e175abbecfe455a32145592bec8ad0f4ed
Robu Constantin Robert <robuconstantin20@stud.ase.ro>
2025-07-14 22:15:26 +0300

    Use upstash for celery manipulation

--- celery_app.py
+++ celery_app.py
@@ -1,9 +1,13 @@
+import os
 from celery import Celery
+
+broker_url = os.getenv("CELERY_BROKER_URL")
+backend_url = os.getenv("CELERY_RESULT_BACKEND", broker_url)
 
 celery = Celery(
     "resume_tasks",
-    broker="redis://localhost:6379/0",      
-    backend="redis://localhost:6379/1"       
+    broker=broker_url,
+    backend=backend_url,
 )
 
 import resume_ingest
