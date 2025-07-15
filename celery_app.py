9443293c32b2f8dfd53d5e41da0d6fe41bcf9112
Robu Constantin Robert <robuconstantin20@stud.ase.ro>
2025-07-15 17:38:09 +0300

    Comment celery worker

--- celery_app.py
+++ celery_app.py
@@ -1,13 +1,13 @@
-import os
-from celery import Celery
+# import os
+# from celery import Celery
 
-broker_url = os.getenv("CELERY_BROKER_URL")
-backend_url = os.getenv("CELERY_RESULT_BACKEND", broker_url)
+# broker_url = os.getenv("CELERY_BROKER_URL")
+# backend_url = os.getenv("CELERY_RESULT_BACKEND", broker_url)
 
-celery = Celery(
-    "resume_tasks",
-    broker=broker_url,
-    backend=backend_url,
-)
+# celery = Celery(
+#     "resume_tasks",
+#     broker=broker_url,
+#     backend=backend_url,
+# )
 
-import resume_ingest
+# import resume_ingest
